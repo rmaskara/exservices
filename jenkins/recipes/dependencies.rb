f5885b2bd27669eda24031ff9644cf58f2cde22a
rakesh maskara <rmaskara@emdeon.com>
2014-02-20 15:23:04 -0600

    commit changes

--- jenkins/recipes/dependencies.rb
+++ jenkins/recipes/dependencies.rb
@@ -1,7 +1,8 @@
 node[:jenkins][:dependencies][:packages].each do |package_name, version|
   package package_name do
-     version {version}
+     version version
 	 action: install
+  end
 end
 
 node[:jenkins][:dependencies][:gems].each do |gem_name, version|
