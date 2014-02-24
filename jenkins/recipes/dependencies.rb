3f94b263fa483004366a728c48c6ea4b2ae105e2
rmaskara <rmaskara@emdeon.com>
2014-02-24 11:43:33 -0600

    Update dependencies.rb

--- jenkins/recipes/dependencies.rb
+++ jenkins/recipes/dependencies.rb
@@ -1,3 +1,3 @@
 node[:jenkins][:dependencies][:gems].each do |gem_name|
-  execute "/usr/local/bin/gem install --no-ri --no-rdoc #{gem_name}" 
+  execute "/usr/local/bin/gem install --no-document #{gem_name}" 
 end
