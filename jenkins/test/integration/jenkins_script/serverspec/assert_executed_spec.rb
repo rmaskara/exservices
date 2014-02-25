c76960121306407c5afb6bdd02e4bf1b05e0256f
rakesh maskara <rmaskara@emdeon.com>
2014-02-25 15:31:55 -0600

    commit changes

--- jenkins/test/integration/jenkins_script/serverspec/assert_executed_spec.rb
+++
@@ -1,7 +0,0 @@
-require_relative '../../../kitchen/data/spec_helper'
-
-describe jenkins_user('sethvargo') do
-  it { should be_a_jenkins_user }
-  it { should have_full_name('Seth Vargo') }
-  it { should have_email('sethvargo@gmail.com') }
-end
