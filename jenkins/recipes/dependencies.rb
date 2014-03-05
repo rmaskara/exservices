179f7cbd75a8035a3a5cad63d37e4e23a856611e
rmaskara <rmaskara@emdeon.com>
2014-03-05 13:42:36 -0600

    Update dependencies.rb

--- jenkins/recipes/dependencies.rb
+++ jenkins/recipes/dependencies.rb
@@ -1,6 +1,6 @@
 node[:jenkins][:dependencies][:gems].each do |gem_name|
   execute "executing gem" do
-    "gem install --no-ri --no-rdoc #{gem_name}" 
-    not_if "gem search --installed #{gem_name}"
+    "/usr/bin/gem install --no-ri --no-rdoc #{gem_name}" 
+    not_if "/usr/bin/gem search --installed #{gem_name}"
   end
 end
