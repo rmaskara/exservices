e8124b0c84ad696876f3a8c644e2e0f4d54c83d8
rmaskara <rmaskara@emdeon.com>
2014-03-07 15:43:47 -0600

    Update default.rb

--- yum/recipes/default.rb
+++ yum/recipes/default.rb
@@ -15,6 +15,7 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
+require 'chefspec'
 
 yum_globalconfig '/etc/yum.conf' do
   cachedir node['yum']['main']['cachedir']
