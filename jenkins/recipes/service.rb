d78e1dae4e0c2ea5b37e8db579b40a111af2420d
rmaskara <rmaskara@emdeon.com>
2014-03-07 23:04:16 -0600

    Update service.rb

--- jenkins/recipes/service.rb
+++ jenkins/recipes/service.rb
@@ -1,9 +1,3 @@
-service 'jenkins init' do
-  supports :restart => true
-  action  [:enable, :start]
-end
-
-
 service 'jenkins' do
   supports :status => true, :restart => true, :reload => true
   action  [:enable, :start]
