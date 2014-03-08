d9b5704dfa1994f709a8deabe53d889ef863f03e
rmaskara <rmaskara@emdeon.com>
2014-03-07 19:28:29 -0600

    Update default.rb

--- yum/recipes/default.rb
+++ yum/recipes/default.rb
@@ -16,6 +16,17 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-template '/etc/yum.conf' do
-  source "yum.conf.erb"
+yum_globalconfig '/etc/yum.conf' do
+  cachedir node['yum']['main']['cachedir']
+  keepcache node['yum']['main']['keepcache']
+  debuglevel node['yum']['main']['debuglevel']
+  exclude node['yum']['main']['exclude']
+  logfile node['yum']['main']['logfile']
+  exactarch node['yum']['main']['exactarch']
+  obsoletes node['yum']['main']['obsoletes']
+  installonly_limit node['yum']['main']['installonly_limit']
+  installonlypkgs node['yum']['main']['installonlypkgs']
+  installroot node['yum']['main']['installroot']
+  distroverpkg node['yum']['main']['distroverpkg']
+  action :create
 end
